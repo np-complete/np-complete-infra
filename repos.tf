a152cc94bd62bd166765c6310a9175af8d20865a
masarakki <masaki182@gmail.com>
2025-05-12 02:02:27 +0900

    add-github-provider

---
+++ repos.tf
@@ -0,0 +1,15 @@
+resource "github_repository" "np-complete-infra" {
+  name                   = "np-complete-infra"
+  visibility             = "public"
+  has_downloads          = false
+  has_discussions        = false
+  has_projects           = false
+  has_wiki               = false
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
+  allow_update_branch    = true
+}
