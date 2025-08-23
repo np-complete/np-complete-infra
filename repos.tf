8a151d5c0ee272467418c36d6141ece981d8bb5e
masarakki <masaki182@gmail.com>
2025-08-23 22:12:25 +0900

    add-build-book-action-repo

--- repos.tf
+++ repos.tf
@@ -13,3 +13,20 @@ resource "github_repository" "np-complete-infra" {
   vulnerability_alerts   = true
   allow_update_branch    = true
 }
+
+resource "github_repository" "build-book-action" {
+  name                   = "build-book-action"
+  description            = "github action to build book"
+  visibility             = "public"
+  has_downloads          = false
+  has_discussions        = false
+  has_projects           = false
+  has_wiki               = false
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+  allow_auto_merge       = true
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
+  allow_update_branch    = true
+}
