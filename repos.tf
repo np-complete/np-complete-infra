aeb0e6f7185222e843945f0253abf392f2d4731e
masarakki <masaki182@gmail.com>
2025-12-30 00:10:23 +0900

    add-branch-rule

--- repos.tf
+++ repos.tf
@@ -14,6 +14,34 @@ resource "github_repository" "np-complete-infra" {
   allow_update_branch    = true
 }
 
+resource "github_repository_ruleset" "np-complete-infra-master-ruleset" {
+  name        = "master"
+  repository  = github_repository.np-complete-infra.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = ["~DEFAULT_BRANCH"]
+      exclude = []
+    }
+  }
+
+  rules {
+    deletion         = true
+    non_fast_forward = true
+    pull_request {
+
+    }
+    required_status_checks {
+      strict_required_status_checks_policy = true
+      required_check {
+        context = "tflint"
+      }
+    }
+  }
+}
+
 resource "github_repository" "build-book-action" {
   name                   = "build-book-action"
   description            = "github action to build book"
